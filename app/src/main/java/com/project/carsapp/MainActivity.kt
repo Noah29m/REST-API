59dc522114df0c2760b3e6ef5eb6d080f55ff3fb
HP <noahmuhindi11@gmail.com>
2023-03-17 16:30:21 +0300

    Rest Api

--- app/src/main/java/com/project/carsapp/MainActivity.kt
+++ app/src/main/java/com/project/carsapp/MainActivity.kt
@@ -3,13 +3,75 @@ package com.project.carsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.project.carsapp.carsapp.MainViewModel
 import com.project.carsapp.ui.theme.CarsappTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+@ExperimentalCoilApi
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CarsappTheme {
+                Column(modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(32.dp)
+
+                ) {
+                    val viewModel: MainViewModel = hiltViewModel()
+                    val car = viewModel.state.value.car
+                    var isLoading = viewModel.state.value.isLoading
+                    car?.let {
+                        Image(painter = rememberImagePainter(data = car.imageUrl,
+                            builder = {
+                                crossfade(true)
+                            }
+
+                            ), contentDescription = "car")
+                        Spacer(modifier = Modifier.height(8.dp))
+                     androidx.compose.material.Text(text = car.name,
+                         fontWeight = FontWeight.Bold,
+                         fontSize = 24.sp
+
+
+                         )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        androidx.compose.material.Text(text = car.description)
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                    }
+                    Button(onClick = viewModel::getRandomCar,
+                        modifier = Modifier.align(Alignment.End)
+
+                    ) {
+                        androidx.compose.material.Text(text = "Next Car")
+                        Spacer(modifier = Modifier.height(8.dp))
+                        if (isLoading){
+                            CircularProgressIndicator()
+                        }
+
+
+                    }
+
+                }
 
             }
         }
--- app/src/main/java/com/project/carsapp/carsapp/MainViewModel.kt
+++ app/src/main/java/com/project/carsapp/carsapp/MainViewModel.kt
@@ -1,11 +1,15 @@
 package com.project.carsapp.carsapp
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.project.carsapp.data.Car
 import com.project.carsapp.data.CarsApi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,8 +19,32 @@ class MainViewModel @Inject constructor(
 
 
 ) : ViewModel() {
-    private val _state = mutableStateOf<CarState>()
-    val : State<CarState> = _state
+    private val _state = mutableStateOf(CarState())
+    val state : State<CarState> = _state
+
+    init {
+        getRandomCar()
+
+    }
+
+    fun getRandomCar(){
+        viewModelScope.launch {
+            try {
+                _state.value = state.value.copy(isLoading = true)
+                _state.value = state.value.copy(
+                    car = api.getRandomCar(),
+                    isLoading = false
+                )
+
+
+            } catch (e:Exception){
+                Log.e("MainViewModel","getRandomCar",e)
+                _state.value = state.value.copy(isLoading = false)
+            }
+
+        }
+
+    }
 
     data class CarState(
         val car: Car? = null,
