1761cb83058700cbda9c2c7b6cfc25a3b9b6cf3a
HP <noahmuhindi11@gmail.com>
2023-04-06 10:34:49 +0300

    :app:checkDebugAarMetadata Issue

--- app/src/main/java/com/project/carsapp/MainActivity.kt
+++ app/src/main/java/com/project/carsapp/MainActivity.kt
@@ -7,16 +7,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.SemanticsProperties.Text
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
+
 import com.project.carsapp.carsapp.MainViewModel
 import com.project.carsapp.ui.theme.CarsappTheme
 import dagger.hilt.android.AndroidEntryPoint
